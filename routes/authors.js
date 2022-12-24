6f4225dc92721401c3bb8528f5dc60f3da2c8afc
Xin chen <czxinchen@Xins-MacBook-Air.local>
2022-12-23 22:59:00 -0700

    initial author routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,20 +1,56 @@
 const express=require('express')
+const author = require('../models/author')
 const router=express.Router()
+const Author=require('../models/author')
 
 
 //Allahthors routes
-router.get('/', (req,res)=>{
-    res.render('authors/index')
+router.get('/', async (req,res)=>{
+    let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+
+        try{
+        const authors= await Author.find(searchOptions)
+        res.render('authors/index',{
+            authors:authors,
+        searchOptions:req.query
+    })
+
+    }catch{
+res.redirect("/")
+    }
+
+    
 })
 
 //new author ruute
 
 router.get('/new', (req,res)=>{
-    res.render('authors/new')
+    res.render('authors/new',{author:new Author()})
 })
 
 //create Author route
-router.post('/',(req,res)=>{
-    res.send('Create')
+router.post('/', (req,res)=>{
+
+
+
+    const author=new Author({
+        name:req.body.name
+    })
+    author.save((err,newAuthor) =>{
+        if(err){
+            res.render('authors/new',{
+                author:author,
+                errorMessage:"error creating Author"
+            }
+
+            )
+        }else {
+            res.redirect('authors')
+        }
+    })
+     
 })
 module.exports=router
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ if(process.env.NODE_ENV !=='production') {
 const express=require('express')
 const app=express()
 const expressLayouts=require('express-ejs-layouts')
+const bodyParser=require('body-parser')
+
 
 const indexRouter=require('./routes/index')
 const authorRouter=require('./routes/authors')
@@ -14,6 +16,8 @@ app.set('views',__dirname +'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
+
 
 const mongoose=require('mongoose')
 
